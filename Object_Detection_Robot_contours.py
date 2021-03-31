ca92a58eb1c0170fd431ade1197bfbbc56f63155
APAC\BHS2SGP <fixed-term.SwapneelDipak.Bhatt@bosch.com>
2021-03-31 12:00:36 +0800

    conveyor belt start stop pickup

--- Object_Detection_Robot_contours.py
+++ Object_Detection_Robot_contours.py
@@ -56,8 +56,8 @@ if __name__ == "__main__":
         mask = utils.objs_mask(frame)
         #cv2.imshow('mask', mask)
         maskCopy = mask.copy()
-        maskCopy = cv2.rotate(maskCopy,cv2.ROTATE_90_COUNTERCLOCKWISE)
-        Display3x3("mask",maskCopy,1)
+        maskCopy = cv2.rotate(maskCopy, cv2.ROTATE_90_COUNTERCLOCKWISE)
+        Display3x3("mask", maskCopy, 1)
 
         frame_copy = frame.copy()
         # drawing region of interest on the color image
@@ -70,7 +70,7 @@ if __name__ == "__main__":
         obj_found = True
         try:
             bounding_box, rect, centre = utils.bounding_box(frame, mask)
-        except :
+        except:
             print("No object detected")
             obj_found = False
             conveyor_belt.turnOn()
@@ -90,66 +90,64 @@ if __name__ == "__main__":
         center_tendency = utils.check_center_tendency(rect)
         if(center_tendency):
             print("Center Object")
-            if ((new_area != prev_area)):
-                print("Area condition satisfied")
-                if(new_slope != prev_slope):
-                    print("Slope condition satisfied")
-                    conveyor_belt.turnOff()
-                    
-                    #relocalise the object
-                    a, frame = utils.take_img(client)
-                    if (frame is None):
-                        continue
-                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    shape = frame.shape
-                    # calculate mask for frame (black and white image)
-                    mask = utils.objs_mask(frame)
-                    #cv2.imshow('mask', mask)
-                    maskCopy = mask.copy()
-                    maskCopy = cv2.rotate(maskCopy,cv2.ROTATE_90_COUNTERCLOCKWISE)
-                    Display3x3("mask",maskCopy,1)
+            if ((new_area != prev_area) or (new_slope != prev_slope)):
+                print("Area or Slope condition satisfied")
+                conveyor_belt.turnOff()
 
-                    frame_copy = frame.copy()
-                    # drawing region of interest on the color image
-                    cv2.line(frame_copy, (0, 220), (200, 220),
-                            (255, 0, 0), thickness_small)
-                    cv2.line(frame_copy, (0, 525), (200, 525),
-                            (0, 0, 255), thickness_small)
-                    #cv2.imshow("Robot Camera frame", frame_copy)
-                    # detect objects using contours and draw box around them
-                    obj_found = True
+                # relocalise the object
+                a, frame = utils.take_img(client)
+                if (frame is None):
+                    continue
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                shape = frame.shape
+                # calculate mask for frame (black and white image)
+                mask = utils.objs_mask(frame)
+                #cv2.imshow('mask', mask)
+                maskCopy = mask.copy()
+                maskCopy = cv2.rotate(maskCopy, cv2.ROTATE_90_COUNTERCLOCKWISE)
+                Display3x3("mask", maskCopy, 1)
 
-                    bounding_box, rect, centre = utils.bounding_box(frame, mask)
-                    new_area = utils.get_area(rect)
-                    new_slope = utils.get_slope(rect)
+                frame_copy = frame.copy()
+                # drawing region of interest on the color image
+                cv2.line(frame_copy, (0, 220), (200, 220),
+                         (255, 0, 0), thickness_small)
+                cv2.line(frame_copy, (0, 525), (200, 525),
+                         (0, 0, 255), thickness_small)
+                #cv2.imshow("Robot Camera frame", frame_copy)
+                # detect objects using contours and draw box around them
+                obj_found = True
 
-                    # use realsense data to get object distance
-                    distance = depth_calculate.distance()
-                    print("Centre of contour is", centre)
-                    print("Distance to object is: {} m from the camera".format(distance))
+                bounding_box, rect, centre = utils.bounding_box(frame, mask)
+                new_area = utils.get_area(rect)
+                new_slope = utils.get_slope(rect)
 
-                    print("Area of Detected object: ", new_area)
-                    print("Slope of Detected object: ", new_slope)
-                    # height = distance of camera to workspace - distance of camera to object top
-                    height = 0.507 - distance
-                    print("Height of object is : {} m".format(height))
-                    prev_area = new_area
-                    prev_slope = new_slope
-                    if obj_found:
-                        key_box.append(bounding_box)
-                        key_frame.append((frame))
-                        obj_found, obj = client.get_target_pose_from_rel(
-                            "workspace", height,  centre[0]/shape[1],centre[1]/shape[0], 0)
-                        print("Relative positions are {},{}".format(centre[0]/shape[1],(centre[1]/shape[0])))
-                        client.pick_from_pose(*obj.to_list())
-                        print("Object Pick Up Pose")
-                        print(obj)
-                        client.place_from_pose(*drop_pose.to_list())
-                    client.move_pose(*observation_pose.to_list())
-                    cv2.destroyAllWindows()
+                # use realsense data to get object distance
+                distance = depth_calculate.distance()
+                print("Centre of contour is", centre)
+                print("Distance to object is: {} m from the camera".format(distance))
+
+                print("Area of Detected object: ", new_area)
+                print("Slope of Detected object: ", new_slope)
+                # height = distance of camera to workspace - distance of camera to object top
+                height = 0.507 - distance
+                print("Height of object is : {} m".format(height))
+                prev_area = new_area
+                prev_slope = new_slope
+                if obj_found:
+                    key_box.append(bounding_box)
+                    key_frame.append((frame))
+                    obj_found, obj = client.get_target_pose_from_rel(
+                        "workspace", height,  centre[0]/shape[1], centre[1]/shape[0], 0)
+                    print("Relative positions are {},{}".format(
+                        centre[0]/shape[1], (centre[1]/shape[0])))
+                    client.pick_from_pose(*obj.to_list())
+                    print("Object Pick Up Pose")
+                    print(obj)
+                    client.place_from_pose(*drop_pose.to_list())
+                client.move_pose(*observation_pose.to_list())
+                cv2.destroyAllWindows()
         cv2.waitKey(1)
         if key == 27:  # Esc key
             client.set_learning_mode(True)
             print("Number of key images: ", len(frame))
             quit()
-
