233e72b7b5b8eb05edca4972ec10894314e29258
Swapneel <swapneel001@e.ntu.edu.sg>
2021-06-25 15:18:50 +0800

    added depth frame test for jinhan

--- depth_frame.py
+++ depth_frame.py
@@ -8,7 +8,7 @@ config = rs.config()
 config.enable_stream(rs.stream.depth,1280,720,rs.format.z16,30)
 config.enable_stream(rs.stream.color,1280,720,rs.format.bgr8,30)
 # config.enable_device_from_file(
-#     "C:/Users/BHS2SGP/Documents/recording2.bag")
+#     "test.bag")
 
 # start reading frames
 # skip first 10 frames to allow auto exposure to adjust
@@ -31,6 +31,15 @@ while True:
     colorizer = rs.colorizer()
     depth_image = np.asanyarray(colorizer.colorize(depth_frame).get_data())
     color_image = np.asanyarray(color_frame.get_data())
-    cv2.imshow("Depth image",depth_image)
+    print("Image shape", depth_image.shape)
+    distance = depth_frame.get_distance(600,500)
+    array = np.empty([720,1280])
+    for i in range(720):
+        for j in range(1280):
+            array[i][j] = depth_frame.get_distance(j,i)
+    print("array shape : ", array.shape)
+    print("distance: ",distance)
+    #cv2.imshow("depth image",depth_image)
     cv2.waitKey(1)
-
+    break
+print("Test distance at a point", array[500][600])
